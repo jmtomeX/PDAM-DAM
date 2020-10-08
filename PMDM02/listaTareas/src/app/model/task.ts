27bd17143b5b16450fc80dcfd2a4b42868f4a954
jmtomeX <jmtomeX@gmail.com>
2020-10-08 18:14:22 +0200

    Comienzo proyecto, creado la clase Task y cambio de color primario

---
+++ PMDM02/listaTareas/src/app/model/task.ts
@@ -0,0 +1,16 @@
+import { stringify } from 'querystring';
+
+import { NullTemplateVisitor } from '@angular/compiler';
+export class Task {
+    task: Task;
+    constructor(
+        public description: string,
+        public isImportant: boolean = false,
+        public finished: boolean = false
+    ) { }
+
+    public static clonaTask(itemTask: Task): Task {
+        return new Task(itemTask.description, itemTask.isImportant, itemTask.finished);
+    }
+
+}
