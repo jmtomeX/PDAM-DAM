dae0ee2c9e2bcf93ec16ebe5caf4a6557a3604a8
jmtomeX <jmtomeX@gmail.com>
2020-10-26 11:12:34 +0100

    cambio fromJson por undefined

--- PMDM03/listaTareas/src/app/model/task.ts
+++ PMDM03/listaTareas/src/app/model/task.ts
@@ -1,10 +1,11 @@
 import { stringify } from 'querystring';
-
 import { NullTemplateVisitor } from '@angular/compiler';
+
 export class Task {
     // añadido por que item.data no se encuentra
     [x: string]: any;
     task: Task;
+
     constructor(
         public id: number = -1,
         public description: string,
@@ -13,10 +14,14 @@ export class Task {
     ) { }
 
     public static fromJson(data): any {
-        if (!data.id || !data.description || typeof data.isImportant !== 'boolean' || typeof data.finished !== 'boolean') {
+        try {
+            if (data.id && data.description && typeof (data.isImportant) !== 'undefined' && typeof (data.finished) !== 'undefined') {
+                return new Task(data.id, data.description, data.isImportant, data.finished);
+            }
             throw (new Error('Argumento no válido: la estructura del argumento no coincide con los campos del modelo'));
+        } catch (e) {
+            console.log((e as Error).message);
         }
-        return new Task(data.id, data.description, data.isImportant, data.finished);
     }
 
     public static cloneTask(itemTask: Task): Task {
